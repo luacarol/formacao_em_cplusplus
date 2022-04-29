d041516e3e4384416d92f4ffa1dbea0b36f2b7e8
luanacaroliny <luana.oliveira@autaza.com>
2022-04-28 22:04:50 -0300

    Fazendo vários chutes

--- FormacaoCPlusPlus/FormacaoCPlusPlus/jogo_da_adivinhacao.cpp
+++ FormacaoCPlusPlus/FormacaoCPlusPlus/jogo_da_adivinhacao.cpp
@@ -14,24 +14,38 @@ int main() {
 	const int NUMERO_SECRETO = 42;
 
 	int chute;
+	bool nao_acertou = true;
+	int tentativas = 0;
 
-	cout << "Qual é o seu chute? ";
-	cin >> chute;
+	while (nao_acertou) {
 
-	cout << "O valor do seu chute é " << chute << endl;
+		tentativas++;
 
-	bool acertou = chute == NUMERO_SECRETO;
-	bool maior = chute > NUMERO_SECRETO;
+		cout << tentativas << "ª tentativa" << endl;
 
-	if (acertou) {
-		cout << "Parabéns, você acertou o número secreto." << endl;
+		cout << "Qual é o seu chute? ";
+		cin >> chute;
+
+		cout << "O valor do seu chute é " << chute << endl;
+
+		bool acertou = chute == NUMERO_SECRETO;
+		bool maior = chute > NUMERO_SECRETO;
+
+		if (acertou) {
+			nao_acertou = false;
+			cout << "Parabéns, você acertou o número secreto." << endl;
+		}
+		else if (maior) {
+			cout << "Seu chute foi maior que o número secreto." << endl;
+		}
+		else {
+			cout << "Seu chute foi menor que o número secreto." << endl;
+		}
+
 	}
-	else if (maior) {
-		cout << "Seu chute foi maior que o número secreto." << endl;
-	}
-	else {
-		cout << "Seu chute foi menor que o número secreto." << endl;
-	}
+
+	cout << "Fim de jogo!" << endl;
+	cout << "Você fez " << tentativas << " tentativas" << endl;
 
 	return 0;
 }
